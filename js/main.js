d21e734f518da1ecd062e11f01deded8d552008f
enesarslan44 <148184884+enesarslan44@users.noreply.github.com>
2023-10-23 13:53:02 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 const inputs = document.querySelectorAll(".input");
 
+window.addEventListener("DOMContentLoaded", function () {
+  const firstInput = document.querySelector("input"); // İlk input elemanını seç
+  if (firstInput) {
+    firstInput.focus(); // Odaklan
+    addcl.call(firstInput); // focus olayını manuel olarak tetikle
+  }
+});
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
 });
